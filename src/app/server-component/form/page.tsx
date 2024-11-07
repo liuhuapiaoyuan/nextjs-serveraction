b3f9a86fcd3f0b92e0c84cb99008b2c15ca9b3f9
liuhuapiaoyuan <278780765@qq.com>
2024-11-07 17:32:23 +0800

    fix: 优化了表单处理逻辑，使用了 `next/form` 组件替换原有表单结构，新增了 `Submit` 异步函数处理提交事件，格式了代码并改善了可读性。

--- src/app/server-component/form/page.tsx
+++ src/app/server-component/form/page.tsx
@@ -1,39 +1,46 @@
 import { PageContainer } from "@/components/ui/PageContainer";
+import Form from "next/form";
 import { redirect } from "next/navigation";
-import Code from './code.mdx';
+import Code from "./code.mdx";
 
-export default async function Page({ searchParams }: { searchParams: Promise<Record<string, string>> }) {
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<Record<string, string>>;
+}) {
   const params = await searchParams;
-  const username = params['username']
-  return <PageContainer
-    title="在`ReactServerComponent/Form`中使用"
-  >
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
-      <div>
-        <Code />
+  const username = params["username"];
+  const onSubmit = async (data: FormData) => {
+    "use server";
+    await new Promise((resolve) => setTimeout(resolve, 200));
+    redirect(
+      `/server-component/form?username=${data.get("username")?.toString()}`
+    );
+  };
+  return (
+    <PageContainer title="在`ReactServerComponent/Form`中使用">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+        <div>
+          <Code />
+        </div>
+        <div>
+          <div> 预览:</div>
+          <Form replace className="grid   gap-2" action={onSubmit}>
+            <label className="flex items-center">
+              <div className="px-2 w-[100px]">用户名：</div>
+              <input type="text" name="username" className="input" />
+            </label>
+            <label className="flex items-center">
+              <div className="px-2 w-[100px]">密码：</div>
+              <input type="password" name="password" className="input" />
+            </label>
+            {username && username.length > 0 && <div>提交数据：{username}</div>}
+            <button data-loading="false" className="btn" type="submit">
+              提交
+            </button>
+          </Form>
+        </div>
       </div>
-      <div >
-        <div> 预览:</div>
-        <form className="grid   gap-2" action={async (data: FormData) => {
-          'use server'
-          console.log('submitting form')
-          await new Promise(resolve => setTimeout(resolve, 200))
-          redirect(`/server-component/form?username=${data.get('username')?.toString()}`)
-        }}>
-          <label className="flex items-center" >
-            <div className="px-2 w-[100px]">用户名：</div>
-            <input type="text" name="username" className="input" />
-          </label>
-          <label className="flex items-center" >
-            <div className="px-2 w-[100px]">密码：</div>
-            <input type="password" name="password" className="input" />
-          </label>
-          {username && username.length > 0 && <div>提交数据：{username}</div>}
-          <button
-            data-loading="false"
-            className="btn" type="submit">提交</button>
-        </form>
-      </div>
-    </div>
-  </PageContainer>
+    </PageContainer>
+  );
 }
