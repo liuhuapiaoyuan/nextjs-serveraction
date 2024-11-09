75336f98fa7b760775f0ac4c65137a58f23ba8f0
liuhuapiaoyuan <278780765@qq.com>
2024-11-09 09:31:39 +0800

    refactor: simplify condition check for item existence in UpdateGoodsPage

--- src/app/crud/@modal/(.)update/[id]/page.tsx
+++ src/app/crud/@modal/(.)update/[id]/page.tsx
@@ -11,17 +11,16 @@ export default async function UpdateGoodsPage({
 }) {
   const { id } = await params;
   const item = await getGoodsById(id);
-  if (!item) {
-    return (
-      <div>
-        商品不存在
-        <Link href="/crud">返回</Link>
-      </div>
-    );
-  }
   return (
     <Modal title="更新商品">
-      <UpdateForm update={updateGoods} item={item} />
+      {item ? (
+        <UpdateForm update={updateGoods} item={item} />
+      ) : (
+        <div>
+          商品不存在
+          <Link href="/crud">返回</Link>
+        </div>
+      )}
     </Modal>
   );
 }
