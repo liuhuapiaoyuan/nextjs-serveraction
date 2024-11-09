e515415aee9cd2b3ef2c0eb47941027341cea4fa
liuhuapiaoyuan <278780765@qq.com>
2024-11-09 09:26:54 +0800

    feat: add 404 handling for update goods page

--- src/app/crud/@modal/(.)update/[id]/page.tsx
+++ src/app/crud/@modal/(.)update/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { Modal } from "../../../modal";
 
 import { UpdateForm } from "@/app/crud/update-form";
+import Link from "next/link";
 import { getGoodsById, updateGoods } from "../../../action";
 
 export default async function UpdateGoodsPage({
@@ -9,9 +10,18 @@ export default async function UpdateGoodsPage({
   params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
+  const item = await getGoodsById(id);
+  if (!item) {
+    return (
+      <div>
+        商品不存在
+        <Link href="/crud">返回</Link>
+      </div>
+    );
+  }
   return (
     <Modal title="更新商品">
-      <UpdateForm update={updateGoods} item={getGoodsById(id)} />
+      <UpdateForm update={updateGoods} item={item} />
     </Modal>
   );
 }
